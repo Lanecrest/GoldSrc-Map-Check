c85652bae7c57629fdb3c54bf2ff5f5ceb26b19f
Lanecrest <126362033+Lanecrest@users.noreply.github.com>
2024-11-24 00:04:32 -0500

    v1_2 update

--- README.md
+++ README.md
@@ -1,28 +1,12 @@
 # GoldSrc Map Check
 
-## Updates
-
-### 1.1 (11-23-24)
-
-+ Updated so that the script no longer need to run in your Half-Life game/mod folders, and can be ran from anywhere.
-+ It will try to autodetect your Steam install of Half-Life to make navigating to the directory quicker
-+ The file will create a new worksheet based on the GoldSrc game/mod parent directory you select.
-+ The file will always output to the directory you run the script in, and you can run it multiple times, meaning you can have multiple worksheets in the same file for each of your GoldSrc games.
-+ The file will generate in alphabetical order by default, with filters.
-+ Better error handling and better comments in the code
-
-### 1.0 (11-23-24)
-
-+ Initial release, uses powershell to create an excel (xlsx) file to check map files in a GoldSrc game/mod folder. Additional features may or may not be implemented in the future. See screenshots for example on what the generated file looks like.
-
-
 ## About
 
-This is a quick and dirty script I made to check any GoldSrc game/mod folders to see if you are missing any .txt, .nav, .res, or overview file for any map (.bsp) for that game. I made this because my Counter-Strike folder was getting out of hand and trying to remember what maps I had overview and nav files for and which ones I didn't.
+This is a quick and dirty script I made to check any GoldSrc game/mod folders to see if you are missing any .txt, .nav, .res, or overview file for any map (.bsp) for that game. I made this because my Counter-Strike folder was getting out of hand and trying to remember what maps I had overview and nav files for and which ones I didn't was becoming complicated.
 
-I also made a column for "Date modified" for the .bsp file so you can quickly check if you are worried if your version's date doesn't seem right (for example, your copy of de_rotterdam seems "too new" so you aren't sure you have the original file or if its a later modification, etc.
+I also made a column for "date modified" and "md5 checksum" for the .bsp file so you can try to determine if your version of a map file is the one you need it to be.
 
-Due to all GoldSrc games using the same folder structures it should work with GoldSrc game/mod but I haven't tested many.
+Due to all GoldSrc games using the same folder structures it should work with any GoldSrc game, but I haven't tested many.
 
 If you have any questions, comments, or ideas for additional features, you can reach out to me on my [GameBanana Profile](https://gamebanana.com/members/2990169)
 
@@ -37,10 +21,31 @@ If you have any questions, comments, or ideas for additional features, you can r
 2. Run "map_check.bat" to help execute "map_check.ps1"
 	- The .bat file was made to help make running the PowerShell script easier. If you know how, you can run the .ps1 file directly, or even copy/paste the contents of the .ps1 file directly into a PowerShell terminal and run it, but the .bat should make it easier for anyone to run.
 	
-3. "map_check.ps1" will create a new file in the directory you run it in named "map_check.xlsx"
+3. "map_check.ps1" will create a new (or update an existing) excel file in the directory you run it in named "map_check.xlsx"
 	- The terminal should tell you what it's doing each step of the way.
 
-4. Open "map_check.xlsx" to see your list of map files and if they have corresponding .txt, .nav, .res, and overview files and the date modified for the .bsp file.
+4. Open "map_check.xlsx" to see your list of map files and if they have corresponding .txt, .nav, .res, and overview files and the date modified and md5 checksum for the .bsp file.
 	- This generated excel file is the file that will help you identify your maps and related files. Open with Microsoft Excel, Google Sheets, etc.
 
-![Google Sheets example](https://github.com/Lanecrest/GoldSrc-Map-Check/blob/main/screenshots/preview.png)
+![Google Sheets example](https://github.com/Lanecrest/GoldSrc-Map-Check/blob/main/screenshots/v1_2_preview.png)
+
+## Updates
+
+### 1.2 (11-24-24)
+
++ User friendly error handling with more information displayed in the terminal as the script runs.
++ Colums will now simply say "yes" if a .nav, .txt, or .res is present, and if it has an overview will just state if it is a .bmp or a .tga
++ Added a checksum column to better help identify if your version of a map is the version you are tying to make sure you have.
+
+### 1.1 (11-23-24)
+
++ Updated so that the script no longer need to run in your Half-Life game/mod folders, and can be ran from anywhere.
++ It will try to autodetect your Steam install of Half-Life to make navigating to the directory quicker.
++ The file will create a worksheet based on the GoldSrc game/mod parent directory you select in an excel workbook.
++ The file will generate in the directory you run the script in, and you can run it multiple times, meaning you can have multiple worksheets in the same workbook for each of your GoldSrc games.
++ The map names will write in alphabetical order by default, with filters.
++ Better error handling and better comments in the code.
+
+### 1.0 (11-23-24)
+
++ Initial release, uses powershell to create an excel (xlsx) file to check map files in a GoldSrc game/mod folder. Additional features may or may not be implemented in the future. See screenshots for example on what the generated file looks like.
--- map_check.ps1
+++ map_check.ps1
@@ -1,5 +1,6 @@
 # Install the ImportExcel module if not already installed
 if (-not (Get-Module -ListAvailable -Name ImportExcel)) {
+	Write-Host "Notice: ImportExcel module is required in order for script to work." -ForegroundColor Red
     Install-Module -Name ImportExcel -Scope CurrentUser -Force
 }
 
@@ -21,12 +22,24 @@ function Find-Folder {
     return $null
 }
 
+# Function to calculate the MD5 checksum of a file
+function Get-FileChecksum {
+    param (
+        [string]$filePath
+    )
+    $md5 = [System.Security.Cryptography.MD5]::Create()
+    $stream = [System.IO.File]::OpenRead($filePath)
+    $checksumBytes = $md5.ComputeHash($stream)
+    $stream.Close()
+    return [BitConverter]::ToString($checksumBytes) -replace '-'
+}
+
 # Search for the default target folder structure and set the default path for the FolderBrowserDialog
 $defaultPath = Find-Folder
 
 # Create and configure the folder browser dialog
 $folderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
-$folderBrowser.Description = "Select the parent folder containing 'maps' and 'overviews'."
+$folderBrowser.Description = "Select a game folder containing 'maps' and 'overviews'."
 
 # Set the default path if the target folder was found
 if ($defaultPath) {
@@ -36,7 +49,7 @@ if ($defaultPath) {
 else {
     Write-Host "Target folder structure not found. Navigate to your Half-Life install folder."
 }
-Write-Host "Select the parent folder containing the desired 'maps' and 'overviews' subfolders."
+Write-Host "Select the game folder containing the desired 'maps' and 'overviews' subfolders."
 
 # Show the browser dialog and starting path
 if ($folderBrowser.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
@@ -44,7 +57,7 @@ if ($folderBrowser.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
     Write-Host "Selected folder: $rootFolder"
 }
 else {
-    Write-Host "No folder selected. Exiting." -ForegroundColor Red
+    Write-Host "No folder selected. Exiting."
     exit
 }
 
@@ -60,7 +73,7 @@ if (-not (Test-Path -Path $mapsFolder)) {
 
 # Check if the "overviews" folder exists
 if (-not (Test-Path -Path $overviewsFolder)) {
-    Write-Host "Error: The 'overviews' folder does not exist in the current directory. Continuing." -ForegroundColor Red
+    Write-Host "Warning: The 'overviews' folder does not exist in the current directory. Continuing." -ForegroundColor Red
 }
 else {
     # Suppress errors if 'overviews' folder doesn't exist or is empty
@@ -71,6 +84,7 @@ else {
 $mapFiles = Get-ChildItem -Path $mapsFolder -File
 
 # Create a hashtable to store the data with the base file name as the key
+Write-Host "Analyzing folders..."
 $fileData = @{}
 
 # Populate the hashtable with .bsp files as the main key
@@ -78,13 +92,14 @@ foreach ($file in $mapFiles) {
     if ($file.Extension -eq ".bsp") {
         $baseName = $file.BaseName
         $fileData[$baseName] = @{
-            "bsp" = $file.Name
+            "bsp" = $file.BaseName
             "txt" = ""
             "nav" = ""
             "res" = ""
             "overview" = ""
             "dateModified" = $file.LastWriteTime
-        }
+			"md5" = Get-FileChecksum -filePath $file.FullName
+		}
     }
 }
 
@@ -93,7 +108,7 @@ foreach ($file in $mapFiles) {
     $baseName = $file.BaseName
     if ($file.Extension -in @(".txt", ".nav", ".res")) {
         if ($fileData.ContainsKey($baseName)) {
-            $fileData[$baseName][$file.Extension.TrimStart(".")] = $file.Name
+            $fileData[$baseName][$file.Extension.TrimStart(".")] = "yes"
         }
     }
 }
@@ -103,24 +118,39 @@ foreach ($file in $overviewFiles) {
     $baseName = $file.BaseName
     if ($file.Extension -in @(".bmp", ".tga")) {
         if ($fileData.ContainsKey($baseName)) {
-            $fileData[$baseName]["overview"] = $file.Name
+            $fileData[$baseName]["overview"] = $file.Extension
         }
     }
 }
 
 # Convert the hashtable to a list of custom objects for Excel export
+Write-Host "Writing excel file..."
 $table = $fileData.GetEnumerator() | ForEach-Object {
     [PSCustomObject]@{
-        ".bsp" = $_.Value["bsp"]
+        "Map" = $_.Value["bsp"]
         ".txt" = $_.Value["txt"]
         ".nav" = $_.Value["nav"]
         ".res" = $_.Value["res"]
-        "overview" = $_.Value["overview"]
-        ".bsp modified" = $_.Value["dateModified"]
+        "Overview" = $_.Value["overview"]
+        "Date Modified" = $_.Value["dateModified"]
+		"MD5 Checksum" = $_.Value["md5"]
     }
-} | Sort-Object -Property ".bsp"  # Sort the data by the .bsp column
+} | Sort-Object -Property "Map"
 
 # Export the data to an Excel file
+$workbookName = "map_check.xlsx"
 $worksheetName = Split-Path -Path $rootFolder -Leaf
-$table | Export-Excel -Path .\map_check.xlsx -WorksheetName $worksheetName -AutoSize -AutoFilter
-Write-Host "Excel file created successfully: map_check.xlsx"
+try {
+    $table | Export-Excel -Path .\$workbookName -WorksheetName $worksheetName -AutoSize -AutoFilter
+    Write-Host "Excel file written successfully: $workbookName"
+}
+catch {
+    # If the file is open or any other issue occurs, handle it gracefully
+    if ($_ -match "Could not open Excel Package") {
+        Write-Host "Error: The file '$workbookName' could not be written to. If it is open, please close the file and try again." -ForegroundColor Red
+    }
+	else {
+        # Generic error handling for other errors
+        Write-Host "An error occurred while creating the Excel file: $_" -ForegroundColor Red
+    }
+}
